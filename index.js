a81c3bfcc6bc329c89419341c8025035c0f43bbc
Ramudgar <ramudgary52@gmail.com>
2025-08-22 09:14:59 +0545

    feture database connection is done

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const db = require("./src/Config/db");
+const config = require("./src/Config/config");
 const app = express();
 
-const port = 5000;
+const port = config.PORT;
 app.use(express.json());
+
+// Connect to the database
+db.connect();
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
